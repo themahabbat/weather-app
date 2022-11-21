e58344cf1472ade195c8e808c32a2960155f952b
themahabbat <mahabbatmusic@gmail.com>
2022-11-22 03:47:36 +0400

    created component for city

---
+++ components/City/index.tsx
@@ -0,0 +1,17 @@
+import { City } from "../../@types/City";
+
+interface Props {
+    city: City,
+    active?: boolean;
+    onClick: () => void
+}
+
+function City({ city, active, onClick }: Props) {
+    return (
+        <div onClick={onClick} className={`${active ? 'bg-indigo-600 text-white' : 'bg-indigo-100'} px-4 py-1 rounded mr-4`}>
+            <span>{city.name}</span>
+        </div>
+    );
+}
+
+export default City;
